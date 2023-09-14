f57631b26f48c7330c116039b7d64db6f18d6964
Jeremy Wizenfeld <jeremy.wizenfeld@gmail.com>
2023-09-14 01:06:31 -0400

    Firebase integration

--- App.js
+++ App.js
@@ -1,10 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import firestore from "@react-native-firebase/firestore";
+import { useEffect, useState } from "react";
 
 export default function App() {
+  doc = firestore().collection("shuls").doc("9IRJbPeBfeJK9H0N9n1A");
+  const [name, setName] = useState("Loading...");
+  const [location, setLocation] = useState("Loading...");
+
+  useEffect(() => {
+    doc
+      .get()
+      .then((docSnapshot) => {
+        if (docSnapshot.exists) {
+          data = docSnapshot.data();
+          setName(data["name"]);
+          setLocation(data["location"]);
+        } else {
+          setName("No document");
+        }
+      })
+      .catch((e) => {
+        console.warn(e);
+        setName("Failed");
+      });
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text>ZmanimNow</Text>
+      <Text>{name}</Text>
+      <Text>{location}</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +39,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
